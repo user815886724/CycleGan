e66afc23bf1b76fe957f34aefd4bf9b750afa4d7
user815886724 <815886724@qq.com>
2021-05-19 21:19:16 +0800

    第一次提交

---
+++ CycleGAN/dataset/__init__.py
@@ -0,0 +1,28 @@
+from torch.utils import data
+import torchvision.transforms as transforms
+from PIL import Image
+import glob
+import os
+import random
+
+
+class ImageDataset(data.Dataset):
+    def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
+        self.transform = transforms.Compose(transforms_)
+        self.unaligned = unaligned
+
+        # 读取文件夹下的文件并排序，文件名称通过排序
+        self.files_A = sorted(glob.glob(os.path.join(root, '%sA' % mode) + '/*.*'))
+        self.files_B = sorted(glob.glob(os.path.join(root, '%sB' % mode) + '/*.*'))
+
+    def __getitem__(self, index):
+        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+        if self.unaligned:
+            item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
+        else:
+            uu = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
+        return {'A': item_A, 'B': item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
+
